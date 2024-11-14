374297a24d28a47617ca7853b58ddd8feaec43fc
Gabriel Danis <danis.gabriel@fronius.com>
2024-11-14 12:04:15 +0100

    changing get campuses to signal

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -8,6 +8,7 @@ import {
   Injector,
 } from '@angular/core';
 import { User } from './../user';
+import { Campus } from './../campus';
 import { ApidataService } from './../apidata.service';
 import { CardComponent } from './../card/card.component';
 import { CardBodyComponent } from './../card-body/card-body.component';
@@ -18,6 +19,13 @@ import { toSignal } from '@angular/core/rxjs-interop';
   standalone: true,
   imports: [CardBodyComponent, CardComponent],
   template: `
+    <section>
+      <ul>
+        @for (capmpus of allCampuses; track campus.id) {
+          <li>{{ campus }}</li>
+        }
+      </ul>
+    </section>
     <section class="wrapper">
       @if (usersSignal(); as users) {
         @for (user of users; track user.id) {
@@ -47,6 +55,7 @@ export class HomeComponent implements OnInit {
   @Input() apiToken!: string;
   usersSignal: Signal<User[] | undefined> = signal(undefined);
   private _injector = inject(Injector);
+  allCampuses!: Promise<Campus[] | []>;
 
   constructor(public apiDataService: ApidataService) {}
   ngOnInit() {
@@ -55,5 +64,21 @@ export class HomeComponent implements OnInit {
       injector: this._injector,
       initialValue: [],
     });
+    this.allCampuses = this.getCampuses();
+  }
+
+  private apiUrl = 'https://api.intra.42.fr';
+
+  async getCampuses(): Promise<Campus[] | []> {
+    const campuses = await fetch(
+      this.apiUrl + '/v2/campus?arg1={page: {number: 2}}',
+      {
+        method: 'GET',
+        headers: {
+          Authorization: `Bearer ${this.apiToken}`,
+        },
+      },
+    );
+    return (await campuses.json()) ?? [];
   }
 }
