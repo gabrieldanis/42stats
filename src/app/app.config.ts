cf8313313530810272967f4a5b849a114c5e26a0
Gabriel Danis <danis.gabriel@fronius.com>
2024-11-21 09:40:46 +0100

    feat: find campus

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,6 +1,5 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter, withComponentInputBinding } from '@angular/router';
-
 import { routes } from './app.routes';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { tokenInterceptor } from './token-interceptor.interceptor';
@@ -10,7 +9,6 @@ export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes, withComponentInputBinding()),
-    // provideHttpClient(),
     provideHttpClient(withInterceptors([tokenInterceptor, delayInterceptor])),
   ],
 };
--- src/app/campuses/campuses.component.ts
+++ src/app/campuses/campuses.component.ts
@@ -1,55 +1,75 @@
 import {
   Component,
+  computed,
   effect,
   inject,
-  Inject,
   input,
   OnInit,
-  signal,
   Signal,
+  signal,
 } from '@angular/core';
 import { FetchCampusesService } from '../fetch-campuses.service';
 import { toObservable, toSignal } from '@angular/core/rxjs-interop';
-import { debounceTime, switchMap, tap, using } from 'rxjs';
+import { switchMap } from 'rxjs';
 import { Router, RouterModule } from '@angular/router';
 import { ApidataService } from '../apidata.service';
+import { FormsModule } from '@angular/forms';
+import { Campus } from '../campus';
 
 @Component({
   selector: 'app-campuses',
   standalone: true,
-  imports: [RouterModule],
-  template: ` @if (campuses(); as campuses) {
-    <section class="campus-content">
-      @for (campus of campuses; track campus.id) {
+  imports: [RouterModule, FormsModule],
+  template: ` <form>
+      <label>Search</label
+      ><input
+        [ngModel]="query()"
+        (ngModelChange)="query.set($event)"
+        [ngModelOptions]="{ standalone: true }"
+        placeholder="start typing"
+      />
+    </form>
+    <p>{{ query() }}</p>
+    @if (foundCampuses(); as campus) {
+      <section class="campus-content">
         <a (click)="goToCampus(campus.id)" class="campus-field">{{
           campus.name
         }}</a>
-      }
-    </section>
-  }`,
+      </section>
+    }
+    ,
+    @if (campuses(); as campuses) {
+      <section class="campus-content">
+        @for (campus of campuses; track campus.id) {
+          <a (click)="goToCampus(campus.id)" class="campus-field">{{
+            campus.name
+          }}</a>
+        }
+      </section>
+    }`,
   styleUrl: './campuses.component.css',
 })
-export class CampusesComponent implements OnInit {
+export class CampusesComponent {
   apiCampusData = inject(FetchCampusesService);
   apiDataService = inject(ApidataService);
   router = inject(Router);
   readonly token = input.required<string>();
-
-  constructor() {
-    // effect(() => console.log(`this is token in campuses: ${this.token()}`));
-  }
-
-  goToCampus(id: number) {
-    const token = this.token();
-    this.router.navigate(['campus/', id], { queryParams: { token } });
-  }
-
-  ngOnInit() {}
+  query = signal('');
   campuses = toSignal(
     toObservable(this.token).pipe(
       switchMap((token) => {
         return this.apiCampusData.fetchAllCampuses(token);
       }),
     ),
+    { initialValue: [] },
   );
+
+  foundCampuses: Signal<Campus | undefined> = computed(() =>
+    this.campuses().find((campus) => campus.name.startsWith(this.query())),
+  );
+
+  goToCampus(id: number) {
+    const token = this.token();
+    this.router.navigate(['campus/', id], { queryParams: { token } });
+  }
 }
